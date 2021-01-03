85b81c91c7d6b7121deee8f1f7d1bde214f196d9
Oumar Diallo <birusod@gmail.com>
2021-01-02 21:38:08 -0800

    Create repo and add folders

---
+++ 00_tidy2021.R
@@ -0,0 +1,14 @@
+
+# List of selected packages for tidytuesday learning:
+
+# 1. Loading data
+
+# 
+# tidyverse / readr
+# vroom
+
+
+
+# links:
+# official repo: https://github.com/rfordatascience/tidytuesday
+# https://github.com/jonthegeek/tidytuesday
